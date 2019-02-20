8905bf95ea0eeb0fde5d5abb54f9756cd7f41a5b
Daniel Tillmann <drtillmann@gmail.com>
2019-02-20 10:47:16 -0600

    Initial commit

---
+++ src/LibraryCode/booktest/Member.java
@@ -0,0 +1,22 @@
+package LibraryCode.booktest;
+
+import java.util.*;
+import java.io.*;
+public class Member {
+
+  private String id;
+   
+  public  Member (String  id) {
+    this.id =  id;
+  }
+
+   
+  public String getId() {
+    return id;
+  }
+   
+  public String toString() {
+    String string =  " id " + id;
+    return string;
+  }
+}
