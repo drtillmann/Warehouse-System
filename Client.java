11d4d68732d869637a69844fcbeaf75631293040
DanielLandon <47797978+DanielLandon@users.noreply.github.com>
2019-03-19 19:56:31 -0500

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,52 @@
+package warehouse.system;
+
+
+import java.util.*;
+import java.io.*;
+public class Client implements Serializable {
+  private static final long serialVersionUID = 1L;
+  private String name;
+  private String address;
+  private String phone;
+  private String id;
+  private static final String CLIENT_STRING = "C";
+  
+  public  Client (String name, String address, String phone) {
+    this.name = name;
+    this.address = address;
+    this.phone = phone;
+    this.id = CLIENT_STRING + (ClientIdServer.instance()).getId();
+  }
+
+  public String getName() {
+    return this.name;
+  }
+  public String getPhone() {
+    return this.phone;
+  }
+  public String getAddress() {
+    return this.address;
+  }
+  public String getId() {
+    return this.id;
+  }
+  public void setName(String newName) {
+    this.name = newName;
+  }
+  public void setAddress(String newAddress) {
+    this.address = newAddress;
+  }
+  public void setPhone(String newPhone) {
+    this.phone = newPhone;
+  }
+   public void setID() {
+    this.id = CLIENT_STRING + (ClientIdServer.instance()).getId();
+  }
+  public boolean equals(String id) {
+    return this.id.equals(id);
+  }
+  public String toString() {
+    String string = "Client name: " + name + ", Address: " + address + ", ID: " + id + ", Phone " + phone;
+    return string;
+  }
+}
---
+++ WaitlistList.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warehouse.system;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author drtil
+ */
+public class WaitlistList implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private List<Waitlist> waitlists = new ArrayList<>();
+    private static WaitlistList waitlistList;
+    
+    public WaitlistList(){
+        
+    }
+    
+    public static WaitlistList instance(){
+        if(waitlistList == null){
+            return waitlistList = new WaitlistList();
+        }else{
+            return waitlistList;
+        }
+    }
+    
+    public boolean addWaitlist(Waitlist wl){
+        this.waitlists.add(wl);
+        return true;
+    }
+    
+    public Iterator getWaitlists(){
+        return this.waitlists.iterator();
+    }
+    
+     // Within WaitlistList
+    public int fulfill(String productID, int quantity, String manu){
+      Iterator iterator = this.getWaitlists();
+       while( iterator.hasNext() ) {
+        Waitlist waitlist = (Waitlist) iterator.next();
+           if (waitlist.needsProduct(productID, manu) && quantity != 0) {
+               quantity = waitlist.decrement(productID, quantity, manu);
+           }
+       }
+       return quantity;
+   }
+    
+    //
+
+    
+   private void writeObject(java.io.ObjectOutputStream output) {
+    try {
+      output.defaultWriteObject();
+      output.writeObject(waitlistList);
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    }
+  }
+  
+  private void readObject(java.io.ObjectInputStream input) {
+    try {
+      if (waitlistList != null) {
+        return;
+      } else {
+        input.defaultReadObject();
+        if (waitlistList == null) {
+          waitlistList = (WaitlistList) input.readObject();
+        } else {
+          input.readObject();
+        }
+      }
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    } catch(ClassNotFoundException cnfe) {
+      cnfe.printStackTrace();
+    }
+  }
+  
+  public String toString(){
+      String sWaitlists = "";
+      for(Waitlist wl : this.waitlists){
+          sWaitlists += wl.toString() + "\n";
+      }
+      return sWaitlists;
+  }
+}
